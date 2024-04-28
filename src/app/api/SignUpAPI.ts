aa555df437409fd540389ee8bac54de933cf731d
LongPC <phamlong992k@gmail.com>
2024-04-28 16:15:26 +0700

    no message

--- src/app/api/SignUpAPI.ts
+++ src/app/api/SignUpAPI.ts
@@ -2,7 +2,7 @@ import {BE_GATEWAY } from "../constant/Constant";
 
 
 const SignUpAPI = async (username: any, password: any, phone: any, email: any) => {
-    const res = await fetch(`${BE_GATEWAY}/tuto-backend/` + "authen/sign-up", {
+    const res = await fetch(`${BE_GATEWAY}/tuto-backend/authen/sign-up`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
--- src/app/auth/signup/page.js
+++ src/app/auth/signup/page.js
@@ -14,18 +14,36 @@ import {
     Heading,
     Text,
     useColorModeValue,
-    Link,
+    Link, FormErrorMessage,
 } from '@chakra-ui/react';
-import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+import {ViewIcon, ViewOffIcon} from '@chakra-ui/icons';
 import {useRouter} from 'next/navigation';
 import {useToast} from '@chakra-ui/react'
+import {Field, Form, Formik} from "formik";
+import SignUpAPI from "../../api/SignUpAPI";
 
 const SignUp = (session) => {
-    const [userInfo, setUserInfo] = useState({username: "", password: ""});
+    const [userInfo, setUserInfo] = useState({username: "", email: "", password: "", rePassword: ""});
     const [showPassword, setShowPassword] = useState(false);
-
-    
-
+    const toast = useToast()
+    const {push} = useRouter();
+    const handleRegisSubmit = async (values, actions) => {
+        const response = await SignUpAPI(values.username, values.password, "", values.email);
+        if (response) {
+            toast({
+                position: "top-right",
+                title: 'Đăng ký thành công',
+                status: 'success',
+                duration: 9000,
+                isClosable: true,
+            })
+            push('/auth/signin');
+        }
+        actions.setSubmitting(false)
+    }
+    const validateRePassword = (value) => {
+        console.log(value)
+    }
     useEffect(() => {
 
     }, []);
@@ -48,63 +66,100 @@ const SignUp = (session) => {
                     boxShadow={'lg'}
                     p={8}>
                     <Stack spacing={4}>
-                        <FormControl id="lastName">
-                            <FormLabel>Tên đăng nhập</FormLabel>
-                            <Input type="text" />
-                        </FormControl>
-                        <FormControl id="email" isRequired>
-                            <FormLabel>Email address</FormLabel>
-                            <Input type="email" />
-                        </FormControl>
-                        <FormControl id="password" isRequired>
-                            <FormLabel>Password</FormLabel>
-                            <InputGroup>
-                                <Input type={showPassword ? 'text' : 'password'} />
-                                <InputRightElement h={'full'}>
-                                    <Button
-                                        variant={'ghost'}
-                                        onClick={() =>
-                                            setShowPassword((showPassword) => !showPassword)
-                                        }>
-                                        {showPassword ? <ViewIcon /> : <ViewOffIcon />}
-                                    </Button>
-                                </InputRightElement>
-                            </InputGroup>
-                        </FormControl>
-                        <FormControl id="password" isRequired>
-                            <FormLabel>Re-Password</FormLabel>
-                            <InputGroup>
-                                <Input type={showPassword ? 'text' : 'password'} />
-                                <InputRightElement h={'full'}>
-                                    <Button
-                                        variant={'ghost'}
-                                        onClick={() =>
-                                            setShowPassword((showPassword) => !showPassword)
-                                        }>
-                                        {showPassword ? <ViewIcon /> : <ViewOffIcon />}
-                                    </Button>
-                                </InputRightElement>
-                            </InputGroup>
-                        </FormControl>
-                        <Stack spacing={10} pt={2}>
-                            <Button
-                                loadingText="Submitting"
-                                size="lg"
-                                bg={'blue.400'}
-                                color={'white'}
-                                _hover={{
-                                    bg: 'blue.500',
-                                }}>
-                                Đăng ký
-                            </Button>
-                        </Stack>
-                        <Stack pt={6}>
-                            <Text align={'center'}>
-                                Already a user? <Link
-                                href={"/auth/signin"}
-                                color={'blue.400'}>Login</Link>
-                            </Text>
-                        </Stack>
+                        <Formik
+                            initialValues={{
+                                name: "",
+                                email: "",
+                                password: "",
+                                rePassword: false
+                            }}
+                            onSubmit={handleRegisSubmit}
+                        >
+                            {(props) => (
+                                <Form>
+                                    <Field name="username">
+                                        {({field, form}) => (
+                                            <FormControl id="username" isRequired>
+                                                <FormLabel>Tên đăng nhập</FormLabel>
+                                                <Input {...field} type="text"/>
+                                            </FormControl>
+                                        )}
+                                    </Field>
+                                    <Field name="email">
+                                        {({field, form}) => (
+                                            <FormControl id="email" isRequired>
+                                                <FormLabel>Email address</FormLabel>
+                                                <Input {...field} type="email"/>
+                                            </FormControl>
+                                        )}
+                                    </Field>
+                                    <Field name="password">
+                                        {({field, form}) => (
+                                            <FormControl id="password" isRequired>
+                                                <FormLabel>Password</FormLabel>
+                                                <InputGroup>
+                                                    <Input {...field} type={showPassword ? 'text' : 'password'}/>
+                                                    <InputRightElement h={'full'}>
+                                                        <Button
+                                                            variant={'ghost'}
+                                                            onClick={() =>
+                                                                setShowPassword((showPassword) => !showPassword)
+                                                            }>
+                                                            {showPassword ? <ViewIcon/> : <ViewOffIcon/>}
+                                                        </Button>
+                                                    </InputRightElement>
+                                                </InputGroup>
+                                            </FormControl>
+                                        )}
+                                    </Field>
+                                    <Field name="rePassword">
+                                        {({field, form}) => (
+                                            <FormControl id="rePassword" isRequired isInvalid={form.errors.rePassword}>
+                                                <FormLabel>Re-Password</FormLabel>
+                                                <InputGroup>
+                                                    <Input {...field} type={showPassword ? 'text' : 'password'}/>
+                                                    <FormErrorMessage>{form.errors.rePassword}</FormErrorMessage>
+                                                    <InputRightElement h={'full'}>
+                                                        <Button
+                                                            variant={'ghost'}
+                                                            onClick={() =>
+                                                                setShowPassword((showPassword) => !showPassword)
+                                                            }>
+                                                            {showPassword ? <ViewIcon/> : <ViewOffIcon/>}
+                                                        </Button>
+                                                    </InputRightElement>
+                                                </InputGroup>
+                                            </FormControl>
+                                        )}
+
+                                    </Field>
+
+                                    <Stack spacing={10} pt={2}>
+                                        <Button
+                                            isLoading={props.isSubmitting}
+                                            loadingText="Submitting"
+                                            size="lg"
+                                            bg={'blue.400'}
+                                            color={'white'}
+                                            type="submit"
+                                            _hover={{
+                                                bg: 'blue.500',
+                                            }}>
+                                            Đăng ký
+                                        </Button>
+                                    </Stack>
+                                    <Stack pt={6}>
+                                        <Text align={'center'}>
+                                            Already a user? <Link
+                                            href={"/auth/signin"}
+                                            color={'blue.400'}>Login</Link>
+                                        </Text>
+                                    </Stack>
+                                </Form>
+                            )}
+
+                        </Formik>
+
                     </Stack>
                 </Box>
             </Stack>
