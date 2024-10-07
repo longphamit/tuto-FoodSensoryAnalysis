35fa59626e76765801605c2ca361cb10e16d2651
LongPC <phamlong992k@gmail.com>
2024-10-07 12:56:55 +0700

    no message

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,14 @@
-const PageIndex=()=>{
-    return(
-        <div>
-            Hello
-        </div>
+import {useRouter} from "next/navigation";
+import {useEffect} from "react";
+
+const PageIndex = () => {
+    const router = useRouter();
+    useEffect(() => {
+        router.replace('/portal');
+    });
+
+    return (
+        <></>
     )
 }
 export default PageIndex
